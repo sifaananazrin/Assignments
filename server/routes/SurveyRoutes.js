4aa2538e666a756d3814f4288154183a2cabe997
Shifana nasrin <shifananazrin15@gmail.com>
2023-12-20 02:37:05 +0530

    Ui completed and its functionality

---
+++ server/routes/SurveyRoutes.js
@@ -0,0 +1,10 @@
+// routes/surveyRoutes.js
+
+const express = require('express');
+const router = express.Router();
+const { validateSurvey } = require('../Middleware/validationMiddleware');
+const surveyController = require('../controllers/surveyController');
+
+router.post('/submit-survey', validateSurvey, surveyController.submitSurvey);
+
+module.exports = router;
---
+++ server/routes/adminRoutes.js
@@ -0,0 +1,11 @@
+// routes/surveyRoutes.js
+
+const express = require('express');
+const router = express.Router();
+const adminController = require('../controllers/adminController');
+
+
+router.post('/adminlogin', adminController.adminLogin);
+router.get('/getallSurveys', adminController.getAllSurveys);
+
+module.exports = router;
